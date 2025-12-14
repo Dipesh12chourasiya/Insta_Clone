698610d804d584664ac0fcc3837dc460527ee58a
Dipesh <dipeshchourasiya12@gmail.com>
2025-12-14 09:51:56 +0530

    project initialized

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_MODE} mode at http://localhost:${PORT}`
+  );
+});
+
